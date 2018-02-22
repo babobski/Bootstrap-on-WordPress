2d2ce9c02a42294340396d3a05b4c477fa408121
Babobski <info@babobski.nl>
2018-02-22 20:18:08 +0100

    fixed bug with navigation

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primaryNav" aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
