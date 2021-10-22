d6fd4f1b8c9bf13e1767f61b5d8b8d260b75fef2
Babobski <info@babobski.nl>
2021-10-22 03:37:15 +0200

    Guttenberg snippet should be disabled by default

--- functions.php
+++ functions.php
@@ -200,17 +200,17 @@
 	======================================================================================================================== */
 
 	// Optional disable guttenberg block editor
-	add_filter( 'use_block_editor_for_post', '__return_false' );
+	// add_filter( 'use_block_editor_for_post', '__return_false' );
 
 
 	//Remove Gutenberg Block Library CSS from loading on the frontend
-	function smartwp_remove_wp_block_library_css() {
-		wp_dequeue_style('wp-block-library');
-		wp_dequeue_style('wp-block-library-theme');
-		wp_dequeue_style('wc-block-style'); // Remove WooCommerce block CSS
-	wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront 
-	}
-	add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
+	// function smartwp_remove_wp_block_library_css() {
+	// 	wp_dequeue_style('wp-block-library');
+	// 	wp_dequeue_style('wp-block-library-theme');
+	// 	wp_dequeue_style('wc-block-style'); // Remove WooCommerce block CSS
+	// wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront 
+	// }
+	// add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
 
 	/* ========================================================================================================================
 
