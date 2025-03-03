9fc094378999332440d07651cfc0d9b85fd89dc9
Babobski | Bob de Haas <info@babobski.nl>
2025-03-03 19:42:18 +0100

    Added dark mode switch fixes #44

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -27,5 +27,32 @@
 				<?php get_search_form(); ?>
 			</div>
 		</div>
+
+		<div class="dropdown ms-3">
+			<button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
+				<span class="theme-icon-active"><i class="bi bi-sun-fill"></i></span>
+				<span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
+			</button>
+			<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
+				<li>
+					<button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="light" aria-pressed="true">
+						<i class="bi bi-sun-fill me-2 opacity-50"></i>
+						Light
+					</button>
+				</li>
+				<li>
+					<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
+						<i class="bi bi-moon-stars-fill me-2 opacity-50"></i>
+						Dark
+					</button>
+				</li>
+				<li>
+					<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="false">
+						<i class="bi bi-circle-half me-2 opacity-50"></i>
+						Auto
+					</button>
+				</li>
+			</ul>
+		</div>
 	</div>
 </nav>
